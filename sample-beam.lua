3a3e7b9a7d8c7ca593a6a65ed42faea36ea6888e
LiyuanLucasLiu <llychinalz@gmail.com>
2016-03-24 20:42:50 +0800

    add beam sample

---
+++ sample-beam.lua
@@ -0,0 +1,44 @@
+require 'torch'
+require 'nn'
+
+require 'LanguageModel'
+
+
+local cmd = torch.CmdLine()
+cmd:option('-checkpoint', 'cv/checkpoint_4000.t7')
+cmd:option('-beam',2,'beam width')
+cmd:option('-beamsample',1,'sample from the beam rather than overwriting the lowest probability candidate')
+cmd:option('-length', 2000)
+cmd:option('-start_text', '')
+cmd:option('-sample', 1)
+cmd:option('-temperature', 1)
+cmd:option('-gpu', 0)
+cmd:option('-gpu_backend', 'cuda')
+cmd:option('-verbose', 0)
+local opt = cmd:parse(arg)
+
+
+local checkpoint = torch.load(opt.checkpoint)
+local model = checkpoint.model
+
+local msg
+if opt.gpu >= 0 and opt.gpu_backend == 'cuda' then
+  require 'cutorch'
+  require 'cunn'
+  cutorch.setDevice(opt.gpu + 1)
+  model:cuda()
+  msg = string.format('Running with CUDA on GPU %d', opt.gpu)
+elseif opt.gpu >= 0 and opt.gpu_backend == 'opencl' then
+  require 'cltorch'
+  require 'clnn'
+  model:cl()
+  msg = string.format('Running with OpenCL on GPU %d', opt.gpu)
+else
+  msg = 'Running in CPU mode'
+end
+if opt.verbose == 1 then print(msg) end
+
+model:evaluate()
+
+local sample = model:sample_beam(opt)
+print(sample)
